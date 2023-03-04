4192e0c9e52e72667ca7c56f09bdff709f331094
SEXY-RIKI-404 <117679399+SEXY-RIKI-404@users.noreply.github.com>
2023-03-04 06:32:33 +0300

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,1 +1,30 @@
-import FILE_CLONE
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from FILE_CLONE import manu
+ 
+        manu()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from FILE_CLONE import manu
+ 
+ 
+        manu()
